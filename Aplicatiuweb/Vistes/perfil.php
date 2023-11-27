1d7c8d19ac6e7e25cf21dbcd2fbc058831db74d9
Miquel <miquelduran@iesmontsia.org>
2023-11-27 19:36:57 +0100

    primera versio de l'estil nou del perfil

--- Aplicatiuweb/Vistes/perfil.php
+++ Aplicatiuweb/Vistes/perfil.php
@@ -50,28 +50,35 @@ if(!isset($_SESSION['usuario_nombre'])) { //en cas que no s'ha iniciat sessió e
 
 <body>
     <div class="container mt-5"> <!-- mostrar per pantalla al usuari el seu nom id i email (modificable) --> 
+    <div class="row">
+        <div class="col">
         <h1>Bienvenido, <?php echo htmlspecialchars($_SESSION['usuario_nombre'], ENT_QUOTES, 'UTF-8'); ?>!</h1>
         <p>ID de Usuario: <?php echo htmlspecialchars($_SESSION['usuario_id'], ENT_QUOTES, 'UTF-8'); ?></p>
         <p>Correo Electrónico: <?php echo htmlspecialchars($_SESSION['usuario_email'], ENT_QUOTES, 'UTF-8'); ?></p>
+        </div> 
+        <div class="col">    
+            <!-- Formulario de actualización -->
+            <h2>Actualizar Datos</h2>
+                
+                    <form method="post" action="/Controladors/controlador_perfil.php" onsubmit="return validarFormulario()"> <!-- en enviar formulari realitzar el script de validació -->
+                        <div class="mb-3">
+                            <label for="nombre" class="form-label">Nombre</label> 
+                            <!-- evitar inserció codi -->
+                            <input type="text" class="form-control" id="nombre" name="nombre" value="<?php echo htmlspecialchars($_SESSION['usuario_nombre'], ENT_QUOTES, 'UTF-8'); ?>">  
+                        </div>
+                        <div class="mb-3">
+                            <label for="email" class="form-label">Correo Electrónico</label>
+                            <input type="email" class="form-control" id="email" name="email" value="<?php echo htmlspecialchars($_SESSION['usuario_email'], ENT_QUOTES, 'UTF-8'); ?>">
+                        </div>
+                        <button type="submit" class="btn btn-primary">Actualizar</button>
+                        <button type="submit" id="cerrar_sesion" name="cerrar_sesion" class="btn btn-secondary">Cerrar Sesión</button>
+                        <button type="submit" id="cerrar_sesion" name="borrar_cuenta" class="btn btn-danger position-absolute end-0 me-5">Borrar Cuenta</button>
 
-        <!-- Formulario de actualización -->
-        <h2>Actualizar Datos</h2>
-        <form method="post" action="/Controladors/controlador_perfil.php" onsubmit="return validarFormulario()"> <!-- en enviar formulari realitzar el script de validació -->
-            <div class="mb-3">
-                <label for="nombre" class="form-label">Nombre</label> 
-                <!-- evitar inserció codi -->
-                <input type="text" class="form-control" id="nombre" name="nombre" value="<?php echo htmlspecialchars($_SESSION['usuario_nombre'], ENT_QUOTES, 'UTF-8'); ?>">  
+                        <div id="mensaje-error" class="alert alert-danger" style="display: none;"></div>
+                    </form>
+                </div>
             </div>
-            <div class="mb-3">
-                <label for="email" class="form-label">Correo Electrónico</label>
-                <input type="email" class="form-control" id="email" name="email" value="<?php echo htmlspecialchars($_SESSION['usuario_email'], ENT_QUOTES, 'UTF-8'); ?>">
-            </div>
-            <button type="submit" class="btn btn-primary">Actualizar</button>
-            <button type="submit" id="cerrar_sesion" name="cerrar_sesion" class="btn btn-secondary">Cerrar Sesión</button>
-            <button type="submit" id="cerrar_sesion" name="borrar_cuenta" class="btn btn-danger position-absolute end-0 me-5">Borrar Cuenta</button>
-
-            <div id="mensaje-error" class="alert alert-danger" style="display: none;"></div>
-        </form>
+    </div>
     </div>
 
     <?php include 'footer.php'; ?>
