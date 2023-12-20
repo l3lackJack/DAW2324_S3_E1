5542c1f0e9f97a1b5820321ca8f48b33f1e8ede3
Gabriel <gabrielionuturs@iesebre.com>
2023-12-20 12:50:21 +0100

    acabo el primer insert automatico

--- fastapi/app/main.py
+++ fastapi/app/main.py
@@ -4,18 +4,29 @@ from typing import List
 import mysql.connector
 import requests
 
+
+
 app = FastAPI()
 
-
 # Definir el modelo de datos para un producto
+class Image(BaseModel):
+    id: int
+    original: str
+    thumb: str
+
 class Producto(BaseModel):
     id: int
-    price: float
     name: str
     variants: int
     sku: str
     dpi: int
     type: str
+    images: List[Image]
+
+    class Config:
+        orm_mode = True
+    
+
 
 api_url = "https://api.picanova.com/api/beta/products"
 usuario = "customaize"
@@ -32,7 +43,6 @@ def obtener_productos():
         # Mostrar la respuesta en formato JSON
         productos = response.json()
         print("Datos recuperados de la API:")
-        print(productos)
 
         return productos
 
@@ -43,54 +53,59 @@ def obtener_productos():
 # Función para insertar productos en la base de datos
 def insertar_en_base_de_datos(productos):
     try:
-        # Conexión a la base de datos
+        # Conexión a mula base de datos
         conn = mysql.connector.connect(
             user="alumne",
             password="alumne1234",
-            database="projectx"
+            database="projectx",
+            host = "localhost",
+            port = "3306"
             )
-        
+        print("Conexion correcta")
+
         cursor = conn.cursor()
+
+        cursor.execute("""DROP TABLE IF EXISTS productos""")
+        cursor.execute("""DROP TABLE IF EXISTS imagenes""")
+        print("Eliminando tablas")
         # Crear tabla de productos (ajusta según la estructura de tus productos)
         cursor.execute("""
             CREATE TABLE IF NOT EXISTS productos (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                name VARCHAR(255),
-                price DOUBLE,
-                sku VARCHAR(255),
-                type VARCHAR(255),
-                variant INTEGER,
-                dpi INTEGER
+                id INTEGER PRIMARY KEY,
+                name VARCHAR(255) NULL,
+                price DOUBLE NULL,
+                sku VARCHAR(255) NULL,
+                type VARCHAR(255) NULL,
+                variants INTEGER NULL,
+                dpi INTEGER NULL
                 )
         """)
-
-
+        print("Create correcto")
         # Insertar productos en la tabla
-        for producto in productos:
-            cursor.execute("""
-                INSERT INTO imagenes (id, id_producto, price, sku, type, variant, dpi)
-                VALUES (?, ?, ?, ?, ?, ?, ?)
-            """, (producto['nombre'], producto['name'], producto['price'], producto['sku'], producto['type'], producto['variant'], producto['dpi']))
+        for producto in productos['data']:
+            print(producto['id'], producto['name'], producto['sku'], producto['type'], producto['variants'], producto['dpi'])
+            sql = "INSERT INTO productos(id, name, sku, type, variants, dpi) VALUES (%s, %s, %s, %s, %s, %s)"
+            val = (producto['id'], producto['name'], producto['sku'], producto['type'], producto['variants'], producto['dpi'])
+            cursor.execute(sql,val)
 
         # Confirmar los cambios
         conn.commit()
 
-        cursor.execute("""
-            CREATE TABLE IF NOT EXISTS imagenes (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                producto_id INT
-                original VARCHAR(255),
-                thumb VARCHAR(255)
-                )
-        """)
-
-        for producto in productos:
-            cursor.execute("""
-                INSERT INTO imagenes (id, id_producto, original, thumb)
-                VALUES (?, ?, ?, ?)
-            """, (producto['images']['id'], producto['id'], producto['images']['thumb'], producto['images']['original']))
+        # cursor.execute("""
+        #     CREATE TABLE IF NOT EXISTS imagenes (
+        #         id INTEGER PRIMARY KEY AUTO_INCREMENT,
+        #         producto_id INT
+        #         original VARCHAR(255),
+        #         thumb VARCHAR(255)
+        #         )
+        # """)
         
-        conn.commit()
+        # for producto in productos:
+        #     cursor.execute("""
+        #         INSERT INTO imagenes (id, id_producto, original, thumb)
+        #         VALUES (?, ?, ?, ?)
+        #     """, (producto['images']['id'], producto['id'], producto['images']['thumb'], producto['images']['original']))
+        
         conn.close()
         print("Productos insertados en la base de datos con éxito.")
     except Exception as e:
