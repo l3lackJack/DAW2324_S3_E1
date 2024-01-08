381625ca6f49796c6b19e9705dd6fcff6267475f
Miquel <miquelduran@iesmontsia.org>
2024-01-08 18:57:16 +0100

    eliminat pagina per actualitzar dades i implementat a la mateixa pagina amb hide i show

--- Aplicatiuweb/Vistes/perfil.php
+++ Aplicatiuweb/Vistes/perfil.php
@@ -1,5 +1,5 @@
-<?php session_start(); //inciar sessió per usar les variables de sessió
-if(!isset($_SESSION['usuario_nombre'])) { //en cas que no s'ha iniciat sessió encara redirigir al login
+<?php session_start();
+if(!isset($_SESSION['usuario_nombre'])) {
     header("Location: /Vistes/login.php");
     exit();
 }
@@ -8,17 +8,16 @@ if(!isset($_SESSION['usuario_nombre'])) { //en cas que no s'ha iniciat sessió e
 <html lang="en">
 
 <?php include 'navbar.php';?>
+
 <head>
-    <script> //validacions javascript per al formulari
+    <script>
         function validarFormulario() {
             var nombre = document.getElementById('nombre').value.trim();
             var email = document.getElementById('email').value.trim();
 
-            // Restablecer colores de fondo a blanco
             document.getElementById('nombre').style.backgroundColor = 'white';
             document.getElementById('email').style.backgroundColor = 'white';
 
-            // Validar campos
             if (nombre === '') {
                 mostrarError('Por favor, complete el campo Nombre.');
                 document.getElementById('nombre').style.backgroundColor = 'rgba(255, 0, 0, 0.2)';
@@ -45,15 +44,19 @@ if(!isset($_SESSION['usuario_nombre'])) { //en cas que no s'ha iniciat sessió e
             div.appendChild(document.createTextNode(str));
             return div.innerHTML;
         }
+
+        function toggleUpdateForm() {
+            var updateFormContainer = document.getElementById('updateFormContainer');
+            updateFormContainer.style.display = (updateFormContainer.style.display === 'none') ? 'block' : 'none';
+        }
     </script>
+
     <script>
-        // Función para mostrar u ocultar el menú desplegable
         function toggleDropdown() {
             var dropdownMenu = document.getElementById('dropdownMenu');
             dropdownMenu.classList.toggle('show');
         }
 
-        // Cerrar el menú desplegable si el usuario hace clic fuera de él
         window.onclick = function(event) {
             if (!event.target.matches('.dropdown-toggle')) {
                 var dropdowns = document.getElementsByClassName('dropdown-menu');
@@ -70,64 +73,63 @@ if(!isset($_SESSION['usuario_nombre'])) { //en cas que no s'ha iniciat sessió e
 
 <body>
     <div class="row">
-    <div class="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark" style="width: 250px; height: 90vh;">
-    <a class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-      <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
-      <span class="fs-4">Perfil</span>
-    </a>
-    <hr>
-    <ul class="nav nav-pills flex-column mb-auto">
-      <li class="nav-item">
-        <a href="#" class="nav-link active" aria-current="page">
-        &nbsp;<svg xmlns="http://www.w3.org/2000/svg" height="32" width="32" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.--><path fill="#ffffff" d="M406.5 399.6C387.4 352.9 341.5 320 288 320H224c-53.5 0-99.4 32.9-118.5 79.6C69.9 362.2 48 311.7 48 256C48 141.1 141.1 48 256 48s208 93.1 208 208c0 55.7-21.9 106.2-57.5 143.6zm-40.1 32.7C334.4 452.4 296.6 464 256 464s-78.4-11.6-110.5-31.7c7.3-36.7 39.7-64.3 78.5-64.3h64c38.8 0 71.2 27.6 78.5 64.3zM256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zm0-272a40 40 0 1 1 0-80 40 40 0 1 1 0 80zm-88-40a88 88 0 1 0 176 0 88 88 0 1 0 -176 0z"/></svg>
-        &nbsp;&nbsp;Datos del Usuario
-        </a>
-      </li>
-      <li>
-        <a href="/Vistes/perfil_actualitzarDades.php" class="nav-link text-white">
-        <svg xmlns="http://www.w3.org/2000/svg" height="32" width="36" viewBox="0 0 576 512"><!--!Font Awesome Free 6.5.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.--><path fill="#ffffff" d="M528 160V416c0 8.8-7.2 16-16 16H320c0-44.2-35.8-80-80-80H176c-44.2 0-80 35.8-80 80H64c-8.8 0-16-7.2-16-16V160H528zM64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H512c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zM272 256a64 64 0 1 0 -128 0 64 64 0 1 0 128 0zm104-48c-13.3 0-24 10.7-24 24s10.7 24 24 24h80c13.3 0 24-10.7 24-24s-10.7-24-24-24H376zm0 96c-13.3 0-24 10.7-24 24s10.7 24 24 24h80c13.3 0 24-10.7 24-24s-10.7-24-24-24H376z"/></svg>
-        &nbsp;&nbsp;Actualizar Datos
-        </a>
-      </li>
-      <li>
-        <a href="#" class="nav-link text-white">
-        &nbsp;<svg xmlns="http://www.w3.org/2000/svg" height="32" width="28" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.--><path fill="#ffffff" d="M50.7 58.5L0 160H208V32H93.7C75.5 32 58.9 42.3 50.7 58.5zM240 160H448L397.3 58.5C389.1 42.3 372.5 32 354.3 32H240V160zm208 32H0V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V192z"/></svg>
-        &nbsp;&nbsp;&nbsp;Mis pedidos
-        </a>
-      </li>
-      <li>
-      <form method="post" action="/Controladors/controlador_perfil.php" onsubmit="return validarFormulario()">
-      <button type="submit" id="cerrar_sesion" name="cerrar_sesion" class="btn btn-secondary">Cerrar Sesión</button>
-      </form>
-    </li>
-    </ul>
-  </div>
-        
-  <div class="col m-4">
-    <h1>Bienvenido, <?php echo htmlspecialchars($_SESSION['usuario_nombre'], ENT_QUOTES, 'UTF-8'); ?>!</h1>
+        <div class="d-flex flex-column flex-shrink-0 p-3 " style="width: 250px; height: 90vh;">
+            <a class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-decoration-none">
+            &nbsp;<svg xmlns="http://www.w3.org/2000/svg" height="32" width="32" viewBox="0 0 512 512"><path fill="#ffffff" d="M406.5 399.6C387.4 352.9 341.5 320 288 320H224c-53.5 0-99.4 32.9-118.5 79.6C69.9 362.2 48 311.7 48 256C48 141.1 141.1 48 256 48s208 93.1 208 208c0 55.7-21.9 106.2-57.5 143.6zm-40.1 32.7C334.4 452.4 296.6 464 256 464s-78.4-11.6-110.5-31.7c7.3-36.7 39.7-64.3 78.5-64.3h64c38.8 0 71.2 27.6 78.5 64.3zM256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zm0-272a40 40 0 1 1 0-80 40 40 0 1 1 0 80zm-88-40a88 88 0 1 0 176 0 88 88 0 1 0 -176 0z"/></svg>
+            &nbsp;&nbsp;<span class="fs-4">Perfil</span>
+            </a>
+            <hr>
+            <ul class="nav nav-pills flex-column mb-auto">
+                <li class="nav-item">
+                        &nbsp;&nbsp;Mis Datos
+                </li>
+                <li>
+                    <form method="post" action="/Controladors/controlador_perfil.php" onsubmit="return validarFormulario()">
+                        <button type="submit" id="cerrar_sesion" name="cerrar_sesion" class="btn btn-secondary">Cerrar Sesión</button>
+                    </form>
+                </li>
+            </ul>
+        </div>
 
-    <?php
-    // Verificar si la variable $_GET['imagen'] está definida
-    if (isset($_GET['imagen'])) {
-        $ruta_destino = htmlspecialchars(urldecode($_GET['imagen']), ENT_QUOTES, 'UTF-8');
-        // Comprobar si la imagen existe
-        echo '<img src="/Aplicatiuweb/img/fotos_perfil/' . basename($ruta_destino) . '" alt="Imagen de perfil" height="300" width="300">';
+        <div class="col m-4">
+            <h1>Bienvenido, <?php echo htmlspecialchars($_SESSION['usuario_nombre'], ENT_QUOTES, 'UTF-8'); ?>!</h1>
 
-    } else {
-        
-    }
-    ?>
+            <?php
+            // Verificar si la variable $_GET['imagen'] está definida
+            if (isset($_GET['imagen'])) {
+                $ruta_destino = htmlspecialchars(urldecode($_GET['imagen']), ENT_QUOTES, 'UTF-8');
+                // Comprobar si la imagen existe
+                echo '<img src="/Aplicatiuweb/img/fotos_perfil/' . basename($ruta_destino) . '" alt="Imagen de perfil" height="300" width="300">';
+            }
+            ?>
 
-    <p>ID de Usuario: <?php echo htmlspecialchars($_SESSION['usuario_id'], ENT_QUOTES, 'UTF-8'); ?></p>
-    <p>Correo Electrónico: <?php echo htmlspecialchars($_SESSION['usuario_email'], ENT_QUOTES, 'UTF-8'); ?></p>
-</div>
+            <p>Nombre: <?php echo htmlspecialchars($_SESSION['usuario_nombre'], ENT_QUOTES, 'UTF-8'); ?></p>
+            <p>Correo Electrónico: <?php echo htmlspecialchars($_SESSION['usuario_email'], ENT_QUOTES, 'UTF-8'); ?></p>
 
+            <button type="button" class="btn btn-primary" onclick="toggleUpdateForm()">Actualizar Datos</button>
 
-    </div>
+            <div id="updateFormContainer" style="display: none;">
+                <h2>Actualizar Datos</h2>
+                <form method="post" action="/Controladors/controlador_perfil.php" onsubmit="return validarFormulario()">
+                    <div class="mb-3">
+                        <label for="nombre" class="form-label">Nombre</label>
+                        <input type="text" class="form-control" id="nombre" name="nombre" value="<?php echo htmlspecialchars($_SESSION['usuario_nombre'], ENT_QUOTES, 'UTF-8'); ?>">
+                    </div>
+                    <div class="mb-3">
+                        <label for="email" class="form-label">Correo Electrónico</label>
+                        <input type="email" class="form-control" id="email" name="email" value="<?php echo htmlspecialchars($_SESSION['usuario_email'], ENT_QUOTES, 'UTF-8'); ?>">
+                    </div>
+
+                    <button type="submit" class="btn btn-primary">Actualizar</button>
+                    <button type="submit" id="cerrar_sesion" name="borrar_cuenta" class="btn btn-danger end-0 me-5">Borrar Cuenta</button>
+
+                    <div id="mensaje-error" class="alert alert-danger" style="display: none;"></div>
+                </form>
+            </div>
+        </div>
     </div>
 
     <?php include 'footer.php'; ?>
-    
 </body>
 
 </html>
