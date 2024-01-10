0ad7cd2223a623d56e9238b2ee551546af004e66
Gabriel <gabrielionuturs@iesebre.com>
2024-01-10 11:35:40 +0100

    estilos no aplicados, aplicacion no funcional

---
+++ Aplicatiuweb/Vistes/choose.php
@@ -0,0 +1,39 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+</head>
+<?php include 'navbar.php'; ?>
+
+<body>
+   
+    <div class="d-flex flex-column align-items-center justify-content-center" style="height: 70vh;">
+        <div class="text-center mb-4">
+            <p class="display-4">Per on començem?</p>
+        </div>
+        <div class="d-flex gap-2">
+            <a href="/Vistes/imagePrompt.php" class="btn btn-outline-secondary d-flex align-items-center btn-lg p-4 w-200">
+                <div class="text-center">
+                    <span class="mx-auto">Imatge</span>
+                </div>
+            </a>
+            <a href="/Vistes/productes.php" class="btn btn-outline-secondary d-flex align-items-center btn-lg p-4 w-200">
+                <div class="text-center">
+                    <span class="mx-auto">Producte</span>
+                </div>
+            </a>
+        </div>
+    </div>
+
+
+    <?php include 'footer.php'; ?>
+</body>
+
+
+
+
+</html>
